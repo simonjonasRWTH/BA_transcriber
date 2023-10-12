7666d26aaa4e65a66e58d41b3e37ad3f09947c7c
Simon Jonas <simon.jonas@rwth-aachen.de>
2023-10-12 17:20:19 +0200

    checkpoint

--- transcribers/tcp.py
+++ transcribers/tcp.py
@@ -1,15 +1,36 @@
+import re
 import transcriber.settings as settings
-from transcriber.messages import IpalMessage
+from transcriber.messages import Activity, IpalMessage
 from transcribers.transcriber import Transcriber
 
-class TCPTranscriber(Transcriber):
+class TcpTranscriber(Transcriber):
     _name = "tcp"
 
-    def matches_protocol(self, pkt):
+    def matches_protocol(self,pkt):
         return "TCP" in pkt
     
-    def parse_packet(self,pkt):
+    def parse_packet(self, pkt):
+        flags = pkt["TCP"].flags.showname_value
+        flags = re.findall(r'\(.*?\)', flags)[0].strip("()").split(",") # set flags in a string like: "SYN, ACK, FIN"
+        flags = {s.strip() for s in flags} # Now a set of each Flag contained in packet
+        
         src = "{}:{}".format(pkt["IP"].src, pkt["TCP"].srcport)
         dest = "{}:{}".format(pkt["IP"].dst, pkt["TCP"].dstport)
+               
+        m = IpalMessage(
+            id = self._id_counter.get_next_id(),
+            timestamp=float(pkt.sniff_time.timestamp()),
+            protocol=self._name,
+            src=src,
+            dest = dest,
+            length=pkt["TCP"].len,
+            crc = int(pkt["TCP"].checksum_status),
+            type = flags,
+            activity=Activity.UNKNOWN,
+            flow="{} - {} - {}".format(src, dest, flags)
+        )
+        # RST is never requested, handle graceful connection termination in match-function
+        m._add_to_request_queue = False if {"RST"} == flags else True
         
-    
+        # everything is a response to something despite connection start
+        m._match_to_requests = False if {"SYN"} == flags else True 
